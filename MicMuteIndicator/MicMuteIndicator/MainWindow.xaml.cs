340e122c818c8033c7b08fdc6d898611e3cbb8ce
Logan Capaldo <logancapaldo@gmail.com>
2016-05-30 07:24:31 -0400

    Display 'muted' when muted. This is basically the whole app.

--- MicMuteIndicator/MicMuteIndicator/MainWindow.xaml.cs
+++ MicMuteIndicator/MicMuteIndicator/MainWindow.xaml.cs
@@ -12,17 +12,50 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+using AudioSwitcher.AudioApi.Observables;
 
 namespace MicMuteIndicator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IObserver<AudioSwitcher.AudioApi.DeviceMuteChangedArgs>
     {
+        private CoreAudioController coreAudioController;
+
+        public static readonly DependencyProperty MutedProperty = DependencyProperty.Register("Muted", typeof(bool), typeof(MainWindow));
+
         public MainWindow()
         {
             InitializeComponent();
+            coreAudioController = new CoreAudioController();
+            Muted = coreAudioController.DefaultCaptureDevice.IsMuted;
+            coreAudioController.DefaultCaptureDevice.MuteChanged.Subscribe(this);
+        }
+        public bool Muted
+        {
+           get { return (bool)GetValue(MutedProperty); }
+           set { SetValue(MutedProperty, (object)value); }
+        }
+
+        public void OnCompleted()
+        {
+        }
+
+        public void OnError(Exception error)
+        {
+            Dispatcher.Invoke(new Action(()=> { throw error; }));
+        }
+
+        public void OnNext(DeviceMuteChangedArgs value)
+        {
+            bool isMuted = value.IsMuted;
+            Dispatcher.Invoke(new Action(() => Muted = isMuted));
         }
     }
+
+    
+
 }
