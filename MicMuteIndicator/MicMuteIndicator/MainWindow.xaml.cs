42eb1eaf59b45122515eb89eb4832a3520c2d34a
Logan Capaldo <logancapaldo@gmail.com>
2016-05-30 08:14:18 -0400

    Initial Release, feature complete. (#1)
    * Add app skeleton
    
    * Add referencen to AudioSwitcher's api
    
    * Display 'muted' when muted. This is basically the whole app.
    
    * Yep, that's all she wrote.

---
+++ MicMuteIndicator/MicMuteIndicator/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+using AudioSwitcher.AudioApi.Observables;
+
+namespace MicMuteIndicator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IObserver<AudioSwitcher.AudioApi.DeviceMuteChangedArgs>
+    {
+        private CoreAudioController coreAudioController;
+
+        public static readonly DependencyProperty MicIsLiveProperty = DependencyProperty.Register("MicIsLive", typeof(bool), typeof(MainWindow));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            coreAudioController = new CoreAudioController();
+            MicIsLive = !coreAudioController.DefaultCaptureDevice.IsMuted;
+            coreAudioController.DefaultCaptureDevice.MuteChanged.Subscribe(this);
+        }
+        public bool MicIsLive
+        {
+           get { return (bool)GetValue(MicIsLiveProperty); }
+           set { SetValue(MicIsLiveProperty, (object)value); }
+        }
+
+        public void OnCompleted()
+        {
+        }
+
+        public void OnError(Exception error)
+        {
+            Dispatcher.Invoke(new Action(()=> { throw error; }));
+        }
+
+        public void OnNext(DeviceMuteChangedArgs value)
+        {
+            bool isMuted = value.IsMuted;
+            Dispatcher.Invoke(new Action(() => MicIsLive = !isMuted));
+        }
+    }
+
+    
+
+}
